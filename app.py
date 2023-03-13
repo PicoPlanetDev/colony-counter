5b21739d9875fa1d1bf469e592ce84f027e96910
PicoPlanetDev <picoplanetdev@gmail.com>
2023-03-13 19:02:57 -0400

    Added info an usability improvements

--- app.py
+++ app.py
@@ -9,16 +9,18 @@ def load_image():
 
     if st.session_state['use_sample_image']:
         image_file = open('sample.jpg', 'rb')
-        st.text('Done playing with the sample image?')
+        st.write('Done playing with the sample image?')
         if st.button('Upload your own'):
             st.session_state['use_sample_image'] = False
             st.experimental_rerun()
     else:
         image_file = st.file_uploader("Upload Images")
-        st.text('Or, try out the sample image:')
+        st.write('Or, try out the sample image:')
         if st.button('Use sample image'):
             st.session_state['use_sample_image'] = True
             st.experimental_rerun()
+
+    st.write('Colony Counter is a tool that allows you to count bacteria colonies in a petri dish. It is recommended that your bacteria colonies are glowing, as I have not tested Colony Counter with tan colonies. I also suggest using a high contrast image, free of glare and shadows. The image can be any resolution, however, Colony Counter will run faster (and is more customizable) with a smaller image. The default settings are optimized for a 1024x1024 image, so please consider resizing it before uploading. Finally, this is a work in progress, so please send feedback to sigkukla@gmail.com.')
 
     # If no image is uploaded, stop the app
     if image_file is None:
@@ -29,7 +31,7 @@ def load_image():
     image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), -1)
 
     # Contrast adjustment option
-    alpha = st.slider('Contrast', 0.0, 3.0, 1.0, 0.1)
+    alpha = st.slider('Contrast', 0.0, 2.0, 1.0, 0.1)
     image = cv2.convertScaleAbs(image, alpha=alpha, beta=0)
     
     # Get grayscale image
@@ -44,7 +46,7 @@ def detect(image, gray):
     ret,thresh = cv2.threshold(gray,thresh_min,thresh_max,0)
 
     # perform an opening operation to remove small chunks
-    kernel_size = st.slider('Kernel size', 0, 20, 3)
+    kernel_size = st.slider('Kernel size', 0, 10, 3)
     morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, np.ones((kernel_size,kernel_size),np.uint8))
 
     # Finding contours
@@ -71,8 +73,17 @@ def detect(image, gray):
     st.download_button(label='Download image', data=file, file_name=f'{num_colonies}_colonies.jpg', mime='image/jpg')
 
 if __name__ == '__main__':
+    st.set_page_config(page_title='Colony Counter', page_icon='🦠', layout='centered', initial_sidebar_state='auto', menu_items={
+        'About': """# Colony Counter
+
+        Created and maintained by Sigmond Kukla 
+        https://sigmondkukla.dev
+        https://github.com/PicoPlanetDev
+        Send bugs and feedback to sigkukla@gmail.com"""
+    })
+
     # Add a title to the app
-    st.title('Colony Counter')
+    st.title('🧫 Colony Counter')
     
     # Prompt to get image
     info = st.info('Please select an image', icon='📷')
