adbd7c99fca3166b684d42e44957c6c56fb2d4e3
PicoPlanetDev <picoplanetdev@gmail.com>
2023-03-15 19:26:45 -0400

    Added cropping and improved UI

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import streamlit as st
+from streamlit_cropper import st_cropper
+from PIL import Image
 
 # @st.cache_data
 def load_image():
@@ -19,6 +21,8 @@ def load_image():
         if st.button('Use sample image'):
             st.session_state['use_sample_image'] = True
             st.experimental_rerun()
+        if image_file:
+            cropped = st_cropper(Image.open(image_file), realtime_update=True, box_color='#61C761', aspect_ratio=(1,1))
 
     st.write('Colony Counter is a tool that allows you to count bacteria colonies in a petri dish. It is recommended that your bacteria colonies are glowing, as I have not tested Colony Counter with tan colonies. I also suggest using a high contrast image, free of glare and shadows. The image can be any resolution, however, Colony Counter will run faster (and is more customizable) with a smaller image. The default settings are optimized for a 1024x1024 image, so please consider resizing it before uploading. Finally, this is a work in progress, so please send feedback to sigkukla@gmail.com.')
 
@@ -27,8 +31,15 @@ def load_image():
         # st.error('No image uploaded', icon='❌')
         st.stop()
 
-    # Get the image as a numpy array
-    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), -1)
+    if st.session_state['use_sample_image']:
+        image = cv2.imread('sample.jpg')
+    else:
+        image = np.array(cropped)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    if image.shape[0] > 1024 or image.shape[1] > 1024:
+        st.warning('Image is very large, resizing to 1024x1024')
+        image = cv2.resize(image, (1024, 1024))
 
     # Contrast adjustment option
     alpha = st.slider('Contrast', 0.0, 2.0, 1.0, 0.1)
@@ -41,9 +52,8 @@ def load_image():
 
 def detect(image, gray):
     # threshold to get binary image
-    thresh_min = st.slider('Threshold min', 0, 255, 120)
-    thresh_max = st.slider('Threshold max', 0, 255, 255)
-    ret,thresh = cv2.threshold(gray,thresh_min,thresh_max,0)
+    threshold_slider = st.slider('Threshold', 0, 255, (120,255))
+    ret,thresh = cv2.threshold(gray,threshold_slider[0],threshold_slider[1],0)
 
     # perform an opening operation to remove small chunks
     kernel_size = st.slider('Kernel size', 0, 10, 3)
@@ -52,10 +62,9 @@ def detect(image, gray):
     # Finding contours
     contours, hierarchy = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    min_area = st.slider('Min area', 0, 100, 1)
-    max_area = st.slider('Max area', 0, 2000, 1000)
-    
-    contours = [contour for contour in contours if cv2.contourArea(contour) >= min_area and cv2.contourArea(contour) <= max_area]
+    area_slider = st.slider('Min area', 0, 3000, (20,1000))
+
+    contours = [contour for contour in contours if cv2.contourArea(contour) >= area_slider[0] and cv2.contourArea(contour) <= area_slider[1]]
     num_colonies = len(contours)
     st.header(f'{num_colonies} colonies detected')
     # print("Number of Contours found = " + str(len(contours)))
